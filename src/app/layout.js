9e39036b38e90b85d912dd5bd55cdb34e1e042b5
Kibirige Calvin <kibscalvin@gmail.com>
2026-01-22 01:35:34 +0300

    Later

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,56 +1,11 @@
 
-import localFont from "next/font/local";
+
 import "./globals.css";
 import Footer from "./components/Footer";
 
 import { TransitionProvider } from "./context/TransitionContext";
 
-const sfPro = localFont({
-  src: [
-    {
-      path: "./fonts/SFPRODISPLAYREGULAR.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/SFPRODISPLAYMEDIUM.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/SFPRODISPLAYBOLD.otf",
-      weight: "700",
-      style: "normal",
-    },
-  ],
-  variable: "--font-sf",
-});
 
-const aktivGrotesk = localFont({
-  src: [
-    {
-      path: "./fonts/AktivGrotesk-Regular.otf",
-      weight: "400",
-      style: "normal",
-    },
-    {
-      path: "./fonts/AktivGrotesk-Medium.otf",
-      weight: "500",
-      style: "normal",
-    },
-    {
-      path: "./fonts/AktivGrotesk-Bold.otf",
-      weight: "700",
-      style: "normal",
-    },
-    {
-        path: "./fonts/AktivGrotesk-XBold.otf",
-        weight: "800",
-        style: "normal",
-      },
-  ],
-  variable: "--font-aktiv",
-});
 
 export const metadata = {
   title: "Weblery - Digital Media Agency | App Development, Web Development & AI Integration",
@@ -62,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${sfPro.variable} ${aktivGrotesk.variable} antialiased font-sans`}
+        className={`antialiased`}
       >
         <TransitionProvider>
             {children}
